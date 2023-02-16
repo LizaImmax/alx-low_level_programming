c419ae99b5c8b0aadeb10afd739ea6ffe6d9d6c6
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-02-16 09:06:36 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stidio.h>
+#include <stdio.h>
 /**
 *main - Entry point
-*Return: Always 0 (sucess)
+*Return: Always 0 (Success)
 */
 
 init main(void)
-
 {
-	put (*\"Programming is like building a multilingual puzzle");
+	put(*\"Programming is like building a multilingual puzzle");
 
-	return (0)
+	(return) (0)
 
 }

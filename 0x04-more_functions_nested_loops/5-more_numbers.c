d9a5b43a3f881ea3c97b4aa3d2773fa6a3852f03
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-02-23 08:54:24 -0800

    55

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ void more_numbers(void)
 {
 	int i, count;
 
-	for (count = 0; count < 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (i = 0; i >= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (i >= 10)
+			if (i > 9)
 			{
-				_putchar(i / 10);
+				_putchar(i / 10 + '0');
 			}
-			_putchar(i % 10);
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}

707a84e29bba9df207e7b9ea8ba0172a50029340
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-03-30 06:38:13 -0700

    trial

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,20 +13,12 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *new = (list_t *)malloc(sizeof(list_t));
 
-	while (str[len])
-		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
-
-	new->str = strdup(str);
-	new->len = len;
+	new->data = *str;
 	new->next = (*head);
+
 	(*head) = new;
 
-	return (*head);
+	return (0);
 }

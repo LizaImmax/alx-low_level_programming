1f915355bd8200db12d2e406a2223a8b14fb627d
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-04-03 08:15:13 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tempo;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		tempo = *head->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = tempo;
+		*head = temp;
 	}
+
+	*head = NULL;
 }

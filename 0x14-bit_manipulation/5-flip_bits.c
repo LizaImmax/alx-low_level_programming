2ad2fd827812c1e9f3a7b8d18aee57cd8657e8a7
LizaImmax <immaxliza@gmail.com>
2023-04-07 11:44:10 +0000

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
+}
+

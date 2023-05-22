7554126d13640bfb63ee4767cd76f34eb79c4401
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-05-22 09:54:50 -0700

    li

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 gcc -Wall -fPIC -c *.c
-gcc -shared -o libdynamic.so *.o
-gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -ldynamic -o len
-
-sudo cp libdynamic.so /usr/lib
+gcc -shared -o liball.so *.o

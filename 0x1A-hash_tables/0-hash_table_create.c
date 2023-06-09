c2fbdd434a4894a9fcedc1a2488c57c61cb17ee1
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-06-08 23:47:35 -0700

    d

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array to be created
+ * hash_table_create - creates a hash table.
+ * @size: size of an array.
  *
- * Return: pointer to the newly created hash table, or NULL on failure
+ * Return: a pointer to the newly created hash table.
+ *         or Null if it fails.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	hash_node_t **new_array;
-	unsigned long int i;
+hash_table_t *hash_table;
+unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-
-	new_array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_array)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		new_array[i] = NULL;
-
-	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
-		return (NULL);
-
-	new_table->size = size;
-	new_table->array = new_array;
-
-	return (new_table);
+if (size == 0)
+return (NULL);
+hash_table = malloc(sizeof(hash_table_t));
+if (hash_table == NULL)
+return (NULL);
+hash_table->size = size;
+hash_table->array = malloc(sizeof(hash_node_t *) * size);
+if (hash_table->array == NULL)
+{
+free(hash_table);
+return (NULL);
+}
+for (i = 0; i < size; i++)
+hash_table->array[i] = NULL;
+return (hash_table);
 }
